694639308b28dfe86e3f9cdd34fdb95b30a78c4a
Tulen <juaustria@gmail.com>
2017-08-27 23:38:59 -0700

    Set up React

---
+++ js/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div className="App"> 
+    <h1> Remindr111 </h1>
+  </div>
+);
+
+export default App;
---
+++ remindr.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './js/app';
+
+const root = document.getElementById('root');
+
+const Root = () => (
+  <App />
+);
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<Root />, root);
+});
+
