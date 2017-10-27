d70c819bbc0adc05b810df8a15c75d9ca85cc6c3
Norris Kwan <norris.kwan@gmail.com>
2017-10-26 18:08:36 -0700

    Add timing inputs

--- js/Add.jsx
+++ js/Add.jsx
@@ -14,7 +14,9 @@ class Add extends Component {
         alert: "",
         // when: new Date().toDateInputValue(),
         // when: Date.now(),
-        when: "",
+        onDay: new Date().toDateInputValue(),
+        atTime: "",
+        occurEvery: "",
         frequency: ""
       },
       currentInput: "alert"
@@ -41,9 +43,12 @@ class Add extends Component {
       if (keyCode == 13) {
         switch (field) {
           case "alert":
-            this.setState({ currentInput: "when" });
+            this.setState({ currentInput: "onDay" });
             break;
-          case "when":
+          case "onDay":
+            this.setState({ currentInput: "atTime" });
+            break;
+          case "atTime":
             this.setState({ currentInput: "frequency" });
             break;
           case "frequency":
@@ -51,15 +56,8 @@ class Add extends Component {
             break;
           case "submit":
             this.props.addReminder(this.state.reminder); // update App state
-            this.setState({
-              // reset this.state.reminder
-              reminder: {
-                alert: "",
-                when: "",
-                frequency: ""
-              },
-              currentInput: "alert"
-            });
+            this.setState({ // reset this.state.reminder
+              reminder: { alert: "", onDay: "", atTime: "", occurEvery: "", frequency: "" }, currentInput: "alert" });
             break;
         }
       }
@@ -68,75 +66,36 @@ class Add extends Component {
   
 
   renderInput() {
-    const { alert, when, frequency } = this.state.reminder;
+    const { alert, onDay, atTime, frequency } = this.state.reminder;
 
     switch (this.state.currentInput) {
       case "alert":
-        return (
-          <input
-            id="alert"
-            type="text"
-            placeholder="Remind me to..."
-            value={alert}
-            onChange={this.update("alert")}
-            onKeyPress={this.submit("alert")}
-            autoFocus
-          />
-        );
-      case "when":
-        return (
-          <div className="label-container">
-            <label>When?</label>
-            <input
-              id="when"
-              type="datetime-local"
-              value={when}
-              onChange={this.update("when")}
-              onKeyPress={this.submit("when")}
-              autoFocus
-            />
-          </div>
-        );
+        return <input id="alert" type="text" placeholder="Remind me to..." value={alert} onChange={this.update("alert")} onKeyPress={this.submit("alert")} autoFocus />;
+      case "onDay":
+        return <div className="label-container">
+            <label>What day?</label>
+            <input id="onDay" type="date" value={onDay} onChange={this.update("onDay")} onKeyPress={this.submit("onDay")} autoFocus />
+          </div>;
+      case "atTime":
+        return <div className="label-container">
+            <label>What time?</label>
+            <input id="onDay" type="time" value={atTime} onChange={this.update("atTime")} onKeyPress={this.submit("atTime")} autoFocus />
+          </div>;
       case "frequency":
-        return (
-          <div className="label-container">
+        return <div className="label-container">
             <label>How often?</label>
             <div onKeyPress={this.submit("frequency")}>
-              <input
-                type="radio"
-                name="frequency"
-                value="hourly"
-                defaultChecked
-              />
+              <input type="radio" name="frequency" value="hourly" defaultChecked />
               <span>
-                Every{" "}
-                <input
-                  id="frequency"
-                  type="number"
-                  placeholder="0"
-                  min="0"
-                  max="9"
-                  value={frequency}
-                  onChange={this.update("frequency")}
-                  onKeyPress={this.submit("frequency")}
-                  autoFocus
-                />{" "}
-                hours.
+                Every <input id="frequency" type="number" placeholder="0" min="0" max="9" value={frequency} onChange={this.update("frequency")} onKeyPress={this.submit("frequency")} autoFocus /> hours.
               </span>
               <input type="radio" name="frequency" value="daily" />Daily
             </div>
-          </div>
-        );
+          </div>;
       case "submit":
-        return (
-          <button
-            className="submit"
-            onKeyPress={this.submit("submit")}
-            autoFocus
-          >
+        return <button className="submit" onKeyPress={this.submit("submit")} autoFocus>
             Gotcha! Press enter to confirm!
-          </button>
-        );
+          </button>;
     }
   }
 
