49ce3c9ef9c034fbc65edef1195e70426a96b838
Norris Kwan <norris.kwan@gmail.com>
2017-12-13 22:28:56 -0600

    Update package.json, prettify Add.jsx

--- js/Add.jsx
+++ js/Add.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import moment from "moment/src/moment";
 
 class Add extends Component {
@@ -7,54 +7,67 @@ class Add extends Component {
     this.state = {
       reminder: {
         alert: "",
-        when: moment.utc(new Date()).format("YYYY-MM-DDT09:00"),
+        when: moment
+          .utc(new Date())
+          .format("YYYY-MM-DDT09:00"),
         frequency: "",
         betweenStart: "09:00",
         betweenEnd: "18:00"
       },
       currentInput: "alert"
     };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this
+      .handleChange
+      .bind(this);
+    this.handleSubmit = this
+      .handleSubmit
+      .bind(this);
   }
 
   handleChange(field) {
     return e => {
       const reminder = this.state.reminder;
       reminder[field] = e.currentTarget.value;
-      this.setState({ reminder });
+      this.setState({reminder});
     };
   }
 
   handleSubmit(field) {
     return e => {
-      if (!e) e = window.event;
-
+      if (!e) 
+        e = window.event;
+      
       const keyCode = e.keyCode || e.which;
 
       if (keyCode == 13) {
         switch (field) {
           case "alert":
-            this.setState({ currentInput: "when" });
+            this.setState({currentInput: "when"});
             break;
           case "when":
-            this.setState({ currentInput: "frequency" });
+            this.setState({currentInput: "frequency"});
             break;
           case "frequency":
-            this.setState({ currentInput: "between" });
+            this.setState({currentInput: "between"});
             break;
           case "between":
-            this.setState({ currentInput: "submit" });
+            this.setState({currentInput: "submit"});
             break;
           case "submit":
             let reminder = this.state.reminder;
-            reminder.when = moment(reminder.when).utc().valueOf();
+            reminder.when = moment(reminder.when)
+              .utc()
+              .valueOf();
             // reminder.frequency = moment(reminder.frequency * 60).utc().valueOf();
-            this.props.addReminder(reminder); // handleChange App state
+            this
+              .props
+              .addReminder(reminder); // handleChange App state
             this.setState({
               reminder: {
                 alert: "",
-                when: moment.utc(new Date()).format("YYYY-MM-DDT09:00"),
+                when: moment
+                  .utc(new Date())
+                  .format("YYYY-MM-DDT09:00"),
                 frequency: "",
                 betweenStart: "09:00",
                 betweenEnd: "18:00"
@@ -66,41 +79,83 @@ class Add extends Component {
       }
     };
   }
-  
 
   renderInput() {
-    const { alert, when, onDay, atTime, frequency, betweenStart, betweenEnd } = this.state.reminder;
+    const {
+      alert,
+      when,
+      onDay,
+      atTime,
+      frequency,
+      betweenStart,
+      betweenEnd
+    } = this.state.reminder;
 
     switch (this.state.currentInput) {
       case "alert":
-        return <input id="alert" type="text" placeholder="Remind me to..." value={alert} onChange={this.handleChange("alert")} onKeyPress={this.handleSubmit("alert")} autoFocus />;
+        return <input
+          id="alert"
+          type="text"
+          placeholder="Remind me to..."
+          value={alert}
+          onChange={this.handleChange("alert")}
+          onKeyPress={this.handleSubmit("alert")}
+          autoFocus/>;
       case "when":
         return <div className="label-container">
-            <label>When?</label>
-            <input id="when" type="datetime-local" value={when} onChange={this.handleChange("when")} onKeyPress={this.handleSubmit("when")} autoFocus />
-          </div>;
+          <label>When?</label>
+          <input
+            id="when"
+            type="datetime-local"
+            value={when}
+            onChange={this.handleChange("when")}
+            onKeyPress={this.handleSubmit("when")}
+            autoFocus/>
+        </div>;
       case "frequency":
         return <div className="label-container">
-            <label>How often?</label>
-            <div>
-              <span>
-                Every <input id="frequency" type="number" placeholder="0" min="0" max="9" value={frequency} onChange={this.handleChange("frequency")} onKeyPress={this.handleSubmit("frequency")} autoFocus /> hours.
-              </span>
-            </div>
-          </div>;
+          <label>How often?</label>
+          <div>
+            <span>
+              Every
+              <input
+                id="frequency"
+                type="number"
+                placeholder="0"
+                min="0"
+                max="9"
+                value={frequency}
+                onChange={this.handleChange("frequency")}
+                onKeyPress={this.handleSubmit("frequency")}
+                autoFocus/>
+              hours.
+            </span>
+          </div>
+        </div>;
       case "between":
         return <div className="label-container">
-            <label>Between?</label>
-            <div>
-              <input id="betweenStart" type="time" value={betweenStart} onChange={this.handleChange("betweenStart")} onKeyPress={this.handleSubmit("between")} autoFocus />
-              &amp;
-              <input id="betweenEnd" type="time" value={betweenEnd} onChange={this.handleChange("betweenEnd")} onKeyPress={this.handleSubmit("between")} />
-            </div>
-          </div>;
+          <label>Between?</label>
+          <div>
+            <input
+              id="betweenStart"
+              type="time"
+              value={betweenStart}
+              onChange={this.handleChange("betweenStart")}
+              onKeyPress={this.handleSubmit("between")}
+              autoFocus/>
+            &amp;
+            <input
+              id="betweenEnd"
+              type="time"
+              value={betweenEnd}
+              onChange={this.handleChange("betweenEnd")}
+              onKeyPress={this.handleSubmit("between")}/>
+          </div>
+        </div>;
       case "submit":
         return <button className="submit" onKeyPress={this.handleSubmit("submit")} autoFocus>
-            Gotcha! Press enter to confirm!
-          </button>;
+          Gotcha! Press enter to confirm!
+        </button>;
     }
   }
 
